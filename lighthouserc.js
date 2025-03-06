a5b6307f767f352ea094a08dc440352ce4ecb2ad
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2025-03-07 01:28:35 +1030

    [40][lighthouse-CI/CD] lighthouse configuration Currently switched off 'unused-javascript' check in lighthouse configuration. But it will definitely be enabled in the future.

--- lighthouserc.js
+++ lighthouserc.js
@@ -10,6 +10,7 @@ module.exports = {
     assert: {
       preset: 'lighthouse:recommended',
       assertions: {
+        'unused-javascript': 'off',
         'categories:performance': ['error', {minScore: 0.9}],
         'categories:accessibility': ['error', {minScore: 0.9}],
         'categories:best-practices': ['error', {minScore: 0.9}],
