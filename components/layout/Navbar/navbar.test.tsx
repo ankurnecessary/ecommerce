899c8c9ca2a5fe98205569bede8caf5e23948ef3
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2025-03-28 11:30:16 +1030

    [34-navigation-desktop] Added navigation bar content 1. Added basic HTML that is needed. 2. Decorated it with required CSS classes. 3. There is nothing dynamic right now in `<Navbar />`. 4. Checked navbar.test.tsx. 5. Added a custom CSS class for left box-shadow.

--- components/layout/Navbar/navbar.test.tsx
+++ components/layout/Navbar/navbar.test.tsx
@@ -5,6 +5,6 @@ import Navbar from '.';
 describe('Navbar', () => {
   it('renders the text inside the component', () => {
     render(<Navbar />);
-    expect(screen.getByText('This is navbar')).toBeInTheDocument();
+    expect(screen.getByText('Categories')).toBeInTheDocument();
   });
 });
