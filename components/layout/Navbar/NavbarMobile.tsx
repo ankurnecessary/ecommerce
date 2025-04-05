f3d86bcabb81c03826efe4baa1116289142932a5
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2025-04-06 03:52:18 +0930

    [34-navigation-desktop]: Mobile navigation Added `<NavbarMobile />`'s footprint but we are going to enable it later on.

---
+++ components/layout/Navbar/NavbarMobile.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavbarMobile = () => {
+  return (
+    <div className="fixed left-0 top-0 h-full w-[300px] border-r border-gray-300">
+      This is mobile navigation.
+    </div>
+  );
+};
+
+export default NavbarMobile;
