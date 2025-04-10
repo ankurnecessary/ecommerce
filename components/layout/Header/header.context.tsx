4a9f1fecaa6911e8da0deb5f37750dbf96e23418
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2025-04-11 03:48:28 +0930

    [34-navigation-desktop]: `useState` to `useReducer` 1. Now we can use `useReducer` to maintain the context value. 2. We can add all the cross component state related variables in it. 3. Added navbar parent and child element to set it's scrolling behaviour.

--- components/layout/Header/header.context.tsx
+++ components/layout/Header/header.context.tsx
@@ -1,9 +1,50 @@
 'use client';
-import React, { createContext, useContext, useState } from 'react';
-import { HeaderContext, MenuVisibility, ToggleMenu } from './types';
+import React, { createContext, useContext, useReducer } from 'react';
+import {
+  HeaderContext,
+  HeaderInitialState,
+  ToggleMenu,
+  SetNavbarElements,
+  HeaderReducer,
+} from '@/components/layout/Header/types';
 
+// Need to use in useReducer() hook
+const headerInitialState: HeaderInitialState = {
+  navbarParentDsktp: null,
+  navbarChildDsktp: null,
+  isMenuVisibleDsktp: [false, ''],
+};
+
+// Need to use in useReducer() hook
+const headerReducer: HeaderReducer = (state, action) => {
+  switch (action.type) {
+    case 'UPDATE_NAVBAR_ELEMENTS_DSKTP':
+      return {
+        ...state,
+        navbarParentDsktp: action.navbarParentDsktp,
+        navbarChildDsktp: action.navbarChildDsktp,
+      };
+    case 'TOGGLE_MENU_DSKTP':
+      return {
+        ...state,
+        isMenuVisibleDsktp: [action.isMenuVisible, action.menuCategory],
+      };
+    default:
+      throw new Error(`Unknown action: ${action.type}`);
+  }
+};
+
+// Created header context
 const headerContext = createContext<HeaderContext>({
-  desktop: { isMenuVisible: [false, ''], toggleMenu: () => {} },
+  desktop: {
+    isMenuVisible: [false, ''],
+    toggleMenu() {},
+    navbar: {
+      parent: null,
+      child: null,
+      setNavbarElements() {},
+    },
+  },
   mobile: null,
 });
 
@@ -12,17 +53,41 @@ export const HeaderContextProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
-  const [isMenuVisible, setIsMenuVisible] = useState<MenuVisibility>([
-    false,
-    '',
-  ]);
+  const [header, dispatchHeaderActions] = useReducer(
+    headerReducer,
+    headerInitialState,
+  );
 
   const toggleMenu: ToggleMenu = (isMenuVisible, menuCategory) => {
-    setIsMenuVisible([isMenuVisible, menuCategory]);
+    dispatchHeaderActions({
+      type: 'TOGGLE_MENU_DSKTP',
+      isMenuVisible,
+      menuCategory,
+    });
+  };
+
+  const setNavbarElements: SetNavbarElements = (
+    navbarParentDsktp,
+    navbarChildDsktp,
+  ) => {
+    // TODO: Check if type is assigned to all the `useState()`
+    dispatchHeaderActions({
+      type: 'UPDATE_NAVBAR_ELEMENTS_DSKTP',
+      navbarParentDsktp,
+      navbarChildDsktp,
+    });
   };
 
   const contextValue = {
-    desktop: { isMenuVisible, toggleMenu },
+    desktop: {
+      isMenuVisible: header.isMenuVisibleDsktp,
+      toggleMenu,
+      navbar: {
+        parent: header.navbarParentDsktp,
+        child: header.navbarChildDsktp,
+        setNavbarElements,
+      },
+    },
     mobile: null,
   };
   return (
--- components/layout/Header/types.ts
+++ components/layout/Header/types.ts
@@ -1,6 +1,35 @@
 export type ToggleMenu = (isMenuVisible: boolean, menuCategory: string) => void;
 export type MenuVisibility = [boolean, string];
+
+export type HeaderInitialState = {
+  navbarParentDsktp: HTMLDivElement | null;
+  navbarChildDsktp: HTMLDivElement | null;
+  isMenuVisibleDsktp: MenuVisibility;
+};
+
+export type SetNavbarElements = (
+  navbarParent: HTMLDivElement,
+  navbarChild: HTMLDivElement,
+) => void;
+
+export type HeaderReducer = (
+  state: HeaderInitialState,
+  action: {
+    type: 'UPDATE_NAVBAR_ELEMENTS_DSKTP' | 'TOGGLE_MENU_DSKTP';
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [key: string]: any;
+  },
+) => HeaderInitialState;
+
 export type HeaderContext = {
-  desktop: { isMenuVisible: MenuVisibility; toggleMenu: ToggleMenu };
+  desktop: {
+    isMenuVisible: MenuVisibility;
+    toggleMenu: ToggleMenu;
+    navbar: {
+      parent: HTMLDivElement | null;
+      child: HTMLDivElement | null;
+      setNavbarElements: SetNavbarElements;
+    };
+  };
   mobile: null;
 };
