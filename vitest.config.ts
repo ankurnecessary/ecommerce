00e2355f38bf34370d011f06d9f602831de3db15
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2025-04-15 08:32:56 +0930

    [34-navigation-desktop][testing]: Configured vitest for @ Earlier vitest was not able to read the imports from `import { Navbar } from '@/components/layout';` because of `@`. But this configuration will enable it to read the paths beginning with `@`.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 import { configDefaults, coverageConfigDefaults, defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -20,5 +21,10 @@ export default defineConfig({
       ]
     },
     setupFiles: ['./vitestSetup.tsx']
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
   },
 });
