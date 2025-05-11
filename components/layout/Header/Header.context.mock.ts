95b9106179b382a90037b0a633baa84be8f4ab5b
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2025-05-11 15:08:54 +0930

    [13-navigation-menu-with-categories] Changed the type definition of the overrides parameter in a more predictive way using `PartialDeep` from `type-fest` to allow for more flexibility in the overrides object. This change improves the type safety and usability of the function, making it easier to work with nested properties in the overrides object.

--- components/layout/Header/Header.context.mock.ts
+++ components/layout/Header/Header.context.mock.ts
@@ -1,18 +1,34 @@
 import { vi } from 'vitest';
+import { PartialDeep } from 'type-fest';
+import { HeaderContext } from '@/components/layout/Header/types';
+import { merge } from 'lodash';
 
-export const mockUseHeaderContext = (overrides = {}) => ({
-  navLinks: [],
-  setNavLinks: vi.fn(),
-  desktop: {
-    isMenuVisible: [false, ''],
-    toggleMenu: vi.fn(),
-    navbar: {
-      child: { getBoundingClientRect: () => ({ width: 5000 }) },
-      parent: { getBoundingClientRect: () => ({ width: 1000 }) },
-      childOffset: 0,
-      setNavbarOffsetDsktp: vi.fn(),
-      setNavbarElementsDsktp: vi.fn(),
-      ...overrides, // Allow overriding specific properties
+export type HeaderContextOverrides = PartialDeep<HeaderContext>;
+
+export const mockUseHeaderContext = (
+  overrides: HeaderContextOverrides = {},
+): HeaderContext => {
+  const defaultContext: HeaderContext = {
+    navLinks: [{ id: 'fkjffh1', href: '/newIn', label: 'New In' }],
+    setNavLinks: vi.fn(),
+    desktop: {
+      isMenuVisible: [false, ''],
+      toggleMenu: vi.fn(),
+      selectedHorizontalNavLink: '',
+      selectedVerticalNavLink: '',
+      verticalNavScrollToElementId: '',
+      setSelectedHorizontalNavLink: vi.fn(),
+      setSelectedVerticalNavLink: vi.fn(),
+      setVerticalNavScrollToElementId: vi.fn(),
+      navbar: {
+        child: null,
+        parent: null,
+        childOffset: 0,
+        setNavbarOffsetDsktp: vi.fn(),
+        setNavbarElementsDsktp: vi.fn(),
+      },
     },
-  },
-});
+    mobile: null
+  };
+  return merge({}, defaultContext, overrides);
+};
