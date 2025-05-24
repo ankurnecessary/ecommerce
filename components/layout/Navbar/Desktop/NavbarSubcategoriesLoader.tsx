d49a424b64a2ab139b33b3ea8fcd764c8a0d4769
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2025-05-25 03:07:27 +0930

    [19-sub-categories-in-navbar-menu] Separated navbar subcategories loader from the actual implementation of `<NavbarSubcategories/>` component.

---
+++ components/layout/Navbar/Desktop/NavbarSubcategoriesLoader.tsx
@@ -0,0 +1,155 @@
+import { Skeleton } from '@/components/ui/skeleton';
+import React from 'react';
+
+const NavbarSubcategoriesLoader = () => {
+  return (
+    <div className="flex flex-wrap gap-10 py-5">
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+      <div>
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <Skeleton className="mt-2 h-3 w-12" />
+      </div>
+    </div>
+  );
+};
+
+export default NavbarSubcategoriesLoader;
