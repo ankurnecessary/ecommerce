308f250cccb4904537d953f05d25a9cc5d88f191
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2025-05-30 04:17:33 +0930

    [stories-detection]: Now all the files ,throughout the project folder, with their name ending with .stories.@(js|jsx|mjs|ts|tsx) will be detected by Storybook.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
-  stories: [
-    '../stories/**/*.mdx',
-    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-  ],
+  stories: ['../**/*.mdx', '../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-onboarding',
     '@chromatic-com/storybook',
