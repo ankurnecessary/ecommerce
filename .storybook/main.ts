7b83fc6948cce3a0bec7a15d8acfe31f800916c8
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2025-06-04 06:13:26 +0930

    [21-storybook-integration] Vitest issue After installing Storybook, when I ran 'npm t' it started testing Storybook files too, which was an unrequired behaviour. So I made these code changes to fix that issue. Now both vitest and Storybook work independently withour intervering with each other.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
+import path from 'node:path';
 
 const config: StorybookConfig = {
   stories: ['../**/*.mdx', '../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -7,11 +8,25 @@ const config: StorybookConfig = {
     '@chromatic-com/storybook',
     '@storybook/addon-docs',
     '@storybook/addon-a11y',
-    '@storybook/addon-vitest',
+    {
+      name: '@storybook/addon-vitest',
+      options: {
+        vitestConfigFile: path.resolve(
+          __dirname,
+          '../vitest.storybook.config.ts',
+        ),
+      },
+    },
   ],
   framework: {
     name: '@storybook/nextjs-vite',
     options: {},
+  },
+  viteFinal: async (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = config.resolve.alias || {};
+    config.resolve.alias['@'] = path.resolve(__dirname, '../');
+    return config;
   },
 };
 export default config;
