a7e5c403b827b549fb7ff922f7a914f83af38679
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2025-06-19 05:17:03 +0930

    [chromatic-installation] Installed chromatic and updated Storybook configuration.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,32 +1,17 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
-import path from 'node:path';
 
 const config: StorybookConfig = {
   stories: ['../**/*.mdx', '../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-onboarding',
     '@chromatic-com/storybook',
     '@storybook/addon-docs',
+    '@storybook/addon-onboarding',
     '@storybook/addon-a11y',
-    {
-      name: '@storybook/addon-vitest',
-      options: {
-        vitestConfigFile: path.resolve(
-          __dirname,
-          '../vitest.storybook.config.ts',
-        ),
-      },
-    },
+    '@storybook/addon-vitest',
   ],
   framework: {
     name: '@storybook/nextjs-vite',
     options: {},
-  },
-  viteFinal: async (config) => {
-    config.resolve = config.resolve || {};
-    config.resolve.alias = config.resolve.alias || {};
-    config.resolve.alias['@'] = path.resolve(__dirname, '../');
-    return config;
   },
 };
 export default config;
