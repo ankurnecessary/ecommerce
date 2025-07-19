d7270104ce222411c9fa41dd224c5fd6e2423c4c
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2025-07-20 06:34:10 +0930

    [navbar-mobile] 1. Made changes in how main mobile menu will render the links. 2. And how sub-menu will be rendered if any main menu link has subcategories available in it.

--- components/layout/Navbar/Mobile/NavbarMobile.tsx
+++ components/layout/Navbar/Mobile/NavbarMobile.tsx
@@ -1,18 +1,32 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { useHeaderContext } from '@/components/layout/Header/Header.context';
-import { HeaderContext } from '@/components/layout/Header/types';
-import { ChevronRight, X } from 'lucide-react';
-import Link from 'next/link';
+import {
+  HeaderContext,
+  MenuSubCategory,
+} from '@/components/layout/Header/types';
+import { ChevronLeft, ChevronRight, X } from 'lucide-react';
 import { links } from '@/components/layout/Navbar/XnavbarLinkObj';
+import clsx from 'clsx';
+import NavbarMobileLink from './NavbarMobileLink';
 
 const NavbarMobile = () => {
+  const [isSubMenuVisible, setIsSubMenuVisible] = useState<boolean>(false);
+  const [subcategories, setSubCategories] = useState<MenuSubCategory[]>([]);
   const {
     mobile: { isMenuVisible, toggleMenu },
   }: HeaderContext = useHeaderContext();
 
-  const mobileMenuHandler = () => {
+  const mainMenuHandler = () => {
     toggleMenu(false);
+    setSubCategories([]);
+    setIsSubMenuVisible(false);
+  };
+
+  // Handling click of category with some sub-categories
+  const categoryClickHandler = (subcategories: MenuSubCategory[]) => {
+    setSubCategories(subcategories);
+    setIsSubMenuVisible(true);
   };
 
   return (
@@ -21,37 +35,88 @@ const NavbarMobile = () => {
       <div
         className={`fixed left-0 top-0 z-[1] h-full w-[300px] border-r border-gray-300 bg-white transition-transform duration-300 ${isMenuVisible ? 'translate-x-0' : '-translate-x-full'}`}
       >
-        <div className="flex justify-end p-2">
-          <button className="bg-slate-200 p-1" onClick={mobileMenuHandler}>
-            <X />
+        {/* START: Button to collapse main mobile menu */}
+        <button
+          className="absolute right-0 top-0 z-10 bg-slate-200 p-1"
+          onClick={mainMenuHandler}
+        >
+          <X />
+        </button>
+        {/* END: Button to collapse main mobile menu */}
+
+        {/* START: Button to collapse sub-menu */}
+        {isSubMenuVisible && (
+          <button
+            className="absolute left-0 top-0 z-10 bg-slate-200 p-1"
+            onClick={() => setIsSubMenuVisible(false)}
+          >
+            <ChevronLeft />
           </button>
-        </div>
-        <div className="h-full overflow-auto">
+        )}
+        {/* END: Button to collapse sub-menu */}
+
+        {/* START: Main menu links */}
+        <div className="mt-9 h-full overflow-auto">
           <ul className="px-2">
             {links.map((link) => (
               <li
                 key={link.id}
-                className="flex justify-between border-b border-dotted border-slate-400 p-2 hover:bg-slate-100"
+                className="border-b border-dotted border-slate-400"
               >
-                <Link href={link.href} className="">
-                  <span>{link.label}</span>
-                </Link>
-                <button className="bg-slate-200 p-1">
-                  <ChevronRight className="h-5 w-5" />
-                </button>
+                {/* START: When we have {subcategories: [...]} */}
+                {link.subcategories?.length && (
+                  <span
+                    className="flex justify-between p-2 hover:bg-slate-100"
+                    onClick={() => categoryClickHandler(link.subcategories)}
+                  >
+                    <span>{link.name}</span>
+                    <button className="bg-slate-200 p-1">
+                      <ChevronRight className="h-5 w-5" />
+                    </button>
+                  </span>
+                )}
+                {/* END: When we have {subcategories: [...]} */}
+
+                {/* START: When we don't have {subcategories: [...]} */}
+                {!link.subcategories?.length && (
+                  <NavbarMobileLink link={link} />
+                )}
+                {/* END: When we don't have {subcategories: [...]} */}
               </li>
             ))}
           </ul>
         </div>
+        {/* END: Main menu links */}
+
+        {/* START: Sub-menu links */}
+        <div
+          className={clsx(
+            'absolute left-0 top-0 z-[2] mt-9 h-full w-full overflow-auto bg-white transition-transform duration-300',
+            { '-translate-x-full': !isSubMenuVisible },
+          )}
+        >
+          <ul className="px-2">
+            {subcategories.map((link) => (
+              <li
+                key={link.id}
+                className="border-b border-dotted border-slate-400"
+              >
+                <NavbarMobileLink link={link} />
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* END: Sub-menu links */}
       </div>
 
-      {/* Translucent Backdrop */}
+      {/* START: Translucent Backdrop */}
       {isMenuVisible && (
         <div
           className="fixed inset-0 z-0 bg-black bg-opacity-50"
-          onClick={mobileMenuHandler}
+          onClick={mainMenuHandler}
         ></div>
       )}
+      {/* END: Translucent Backdrop */}
     </>
   );
 };
