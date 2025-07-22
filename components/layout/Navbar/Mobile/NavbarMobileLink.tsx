ad10137888465ac2c5b6af9e025c4a89a9142bd4
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2025-07-23 00:14:53 +0930

    [prefetch] Just added prefetch to remove errors from the console.

--- components/layout/Navbar/Mobile/NavbarMobileLink.tsx
+++ components/layout/Navbar/Mobile/NavbarMobileLink.tsx
@@ -5,9 +5,11 @@ type navbarMobileLink = { link: { name: string; url: string } };
 
 const NavbarMobileLink = ({ link }: navbarMobileLink) => {
   return (
+    // [ ]: Remove prefetch false when the related page is added.
     <Link
       href={link.url}
       className="inline-block w-full px-2 py-[10px] hover:bg-slate-100"
+      prefetch={false}
     >
       <span>{link.name}</span>
     </Link>
