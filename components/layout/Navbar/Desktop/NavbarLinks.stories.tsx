c0a9ddddee59cb6474d6432748b204f1a2c2fccf
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2025-08-20 03:44:12 +0930

    [storybook-tests] Added storybook test cases to the <NavbarLinks /> stories.

--- components/layout/Navbar/Desktop/NavbarLinks.stories.tsx
+++ components/layout/Navbar/Desktop/NavbarLinks.stories.tsx
@@ -3,6 +3,7 @@ import NavbarLinks from '@/components/layout/Navbar/Desktop/NavbarLinks';
 import { fn } from 'storybook/test';
 import type { StoryObj } from '@storybook/nextjs-vite';
 import MockHeaderContextProvider from '@/components/layout/Header/Header.context.stories.mock';
+import { expect } from 'storybook/test';
 
 const meta = {
   title: 'components/layout/Navbar/Desktop/NavbarLinks',
@@ -36,6 +37,26 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const links = [
+  {
+    id: 'fkjffh1',
+    url: '/newIn',
+    name: 'New In',
+    subcategories: [],
+  },
+  {
+    id: 'fkjffh2',
+    url: '/sale',
+    name: 'Sale',
+    subcategories: [],
+  },
+  {
+    id: 'fkjffh3',
+    url: '/womenClothing',
+    name: 'Women Clothing',
+    subcategories: [],
+  },
+];
 export const Default: Story = {
   args: {
     mouseOverHandler: () => () => {},
@@ -43,30 +64,17 @@ export const Default: Story = {
   },
   parameters: {
     overrides: {
-      navLinks: [
-        {
-          id: 'fkjffh1',
-          url: '/newIn',
-          name: 'New In',
-          subcategories: [],
-        },
-        {
-          id: 'fkjffh2',
-          url: '/sale',
-          name: 'Sale',
-          subcategories: [],
-        },
-        {
-          id: 'fkjffh3',
-          url: '/womenClothing',
-          name: 'Women Clothing',
-          subcategories: [],
-        },
-      ],
+      navLinks: links,
       desktop: {
-        selectedHorizontalNavLink: 'Sale',
+        selectedHorizontalNavLink: links[1].name,
       },
     },
+  },
+  play: async ({ canvas }) => {
+    const linkElements = canvas.queryAllByRole('link');
+    await expect(linkElements.length).toBe(links.length);
+    const link = canvas.getByText(links[1].name);
+    await expect(link).toHaveClass('bg-gray-100');
   },
 };
 
@@ -79,6 +87,10 @@ export const Loading: Story = {
     overrides: {
       navLinks: [],
     },
+  },
+  play: async ({ canvas }) => {
+    const linkElements = canvas.queryAllByRole('link');
+    await expect(linkElements.length).toBe(0);
   },
 };
 
